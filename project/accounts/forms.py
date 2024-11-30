f6b4e3d65c23fdf10ae85d71d6e0ed7f847ddab9
tred-07 <aranna.rtx.bd.02@gmail.com>
2024-11-30 18:07:33 +0600

    upd

--- project/accounts/forms.py
+++ project/accounts/forms.py
@@ -12,4 +12,31 @@ class UserRegistrationForm(UserCreationForm):
     country=forms.CharField(max_length=40)
     class Meta:
         model=User
-        fields=['username','password1','password2','first_name','last_name','email']
+        fields=['username','password1','password2','first_name','last_name','email','birth_date','gender','street_address','city','postal_code','country']
+
+    def save(self,commit=True):
+        our_user=super().save(commit=False)
+        if(commit==True):
+            our_user.save() # save data user model
+            account_type=self.cleaned_data('account_type')
+            birth_date=self.cleaned_data.get('birth_date')
+            gender=self.cleaned_data.get('gender')
+            street_address=self.cleaned_data.get('street_address')
+            city=self.cleaned_data('city')
+            postal_code=self.cleaned_data('postal_code')
+            country=self.cleaned_data('country') 
+            UserAddress.objects.create(
+                user=our_user,
+                street_address=street_address,
+                postal_code=postal_code,
+                country=country,
+            )
+            UserBankModel.objects.create(
+                user=our_user,
+                account_no=1000+our_user.id,
+                account_type=account_type,
+                birth_date=birth_date,
+                gender=gender,
+                city=city
+            )
+        return our_user
--- project/accounts/views.py
+++ project/accounts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.views.generic import FormView
+from django.contrib.auth import login,logout
+from .forms import UserRegistrationForm
 # Create your views here.
+class UserRegistrationView(FormView):
+    template_name=''
+    form_class=UserRegistrationForm
+    success_url=''
+    def form_valid(self, form):
+        user=form.save()
+        login(user)
+        return super().form_valid(form)
